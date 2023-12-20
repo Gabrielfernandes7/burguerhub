4e53f20c36f01e98e903c292648ea2ec3ea24f00
Gabriel Fernandes <78227127+Gabrielfernandes7@users.noreply.github.com>
2023-12-20 02:23:25 -0300

    feat: footer (#3)

---
+++ src/components/atoms/FooterComponent/FooterComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <div
+        class="bg-dark"
+        id="footer">
+      <p>Faça seu Hamburger &copy; 2021</p>
+    </div>
+</template>
+  
+<style scoped>
+#footer {
+    position: absolute;
+    height: 100px;
+    border-top: 4px solid #111;
+    color: #fcba03;
+    
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    bottom: 0;
+    width: 100%;
+}
+</style>
