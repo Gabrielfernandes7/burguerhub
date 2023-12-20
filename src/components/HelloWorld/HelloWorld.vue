b8a4c52eff470e07887fa9a5a64971f248563eaf
Gabriel Fernandes <gabriel.assuncao.fernandes07@aluno.ifce.edu.br>
2023-12-20 02:20:15 -0300

    feat: footer

--- src/components/HelloWorld/HelloWorld.vue
+++ src/components/HelloWorld/HelloWorld.vue
@@ -1,10 +1,15 @@
 <script setup>
 import NavbarComponent from 
   "@/components/templates/NavbarComponent/NavbarComponent.vue"
+
+import FooterComponent from 
+  "@/components/atoms/FooterComponent/FooterComponent.vue"
+
 </script>
 
 <template>
   <NavbarComponent>
     <div>Hello World</div>
+    <FooterComponent />
   </NavbarComponent>
 </template>
