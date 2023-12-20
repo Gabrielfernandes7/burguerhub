6826add28e1b507ec7ae5f1a5e923425d38b269c
Gabriel Fernandes <78227127+Gabrielfernandes7@users.noreply.github.com>
2023-12-20 14:39:31 -0300

    feat: home (#4)

---
+++ src/components/pages/HomePage/HomePage.vue
@@ -0,0 +1,18 @@
+<script setup>
+import NavbarTemplate from 
+  "../../templates/NavbarComponent/NavbarTemplate.vue"
+
+import FooterComponent from
+  "../../atoms/FooterComponent/FooterComponent.vue"
+import BannerHome from "@/components/atoms/BannerHome/BannerHome.vue";
+
+</script>
+
+<template>
+  <NavbarTemplate>
+    <main>
+      <BannerHome />
+    </main>
+    <FooterComponent />
+  </NavbarTemplate>
+</template>
