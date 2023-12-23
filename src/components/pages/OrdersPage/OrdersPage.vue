02abcff39961f438ab3ada21d0e565bdf0b40ff2
Gabriel Fernandes <78227127+Gabrielfernandes7@users.noreply.github.com>
2023-12-22 21:03:12 -0300

    feat: orders page (#6)

---
+++ src/components/pages/OrdersPage/OrdersPage.vue
@@ -0,0 +1,12 @@
+<script setup>
+import NavbarTemplate from 
+  "../../templates/NavbarComponent/NavbarTemplate.vue"
+</script>
+
+<template>
+    <NavbarTemplate>
+        <main>
+            
+        </main>
+    </NavbarTemplate>
+</template>
