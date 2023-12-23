5c7fdf81db328d929b926018fc89fb8cc16dbe06
Gabriel Fernandes <gabriel.assuncao.fernandes07@aluno.ifce.edu.br>
2023-12-22 21:42:24 -0300

    feat: dashboard

--- src/components/pages/OrdersPage/OrdersPage.vue
+++ src/components/pages/OrdersPage/OrdersPage.vue
@@ -1,12 +1,15 @@
 <script setup>
 import NavbarTemplate from 
   "../../templates/NavbarComponent/NavbarTemplate.vue"
+
+import DashboardComponent from
+  "@/components/organisms/Dashboard/Dashboard.vue"
 </script>
 
 <template>
     <NavbarTemplate>
         <main>
-            
+            <DashboardComponent />
         </main>
     </NavbarTemplate>
 </template>
