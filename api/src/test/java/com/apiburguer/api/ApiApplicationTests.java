faf1208c08ae5ee68d2b9d39b7b1de141ac2ba4b
Gabriel Fernandes <gabrielassuncaofernandes@gmail.com>
2025-01-01 15:02:34 -0300

    modify backend framework to spring

---
+++ api/src/test/java/com/apiburguer/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiburguer.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
